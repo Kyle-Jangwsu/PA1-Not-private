2e0891f18ee1c5b9e8abf8d9c268e54eea6e943d
Kyle-Jangwsu <122514272+Kyle-Jangwsu@users.noreply.github.com>
2023-10-05 16:49:10 -0700

    Update list.hpp

--- list.hpp
+++ list.hpp
@@ -1,4 +1,8 @@
-#include "resource.hpp"
+using namespace std;
+#include <fstream>
+#include <string>
+#include <iostream>
+//#include "resources.hpp"
 template <class T>
 struct Node
 {
@@ -21,52 +25,3 @@ void Display(int num); // function to display all the commands
 private:
 Node<T>* pHead;
 };
-
-template <class T>
-void List<T>::add(T word, T description)
-{
-    Node<T>* pNode = new Node<T>;
-    pNode->wordCommand = word;
-    pNode->wordDescription = description;
-    pNode->next = pHead;
-    pHead = pNode;
-}
-
-template <class T>
-void List<T>::remove(T word)
-{
-    Node<T>* pCur = pHead;
-    Node<T>* pPrev = NULL;
-    while (pCur != NULL)
-    {
-        if (pCur->Word == word)
-        {
-            if(pCur == pHead)
-            {
-                pHead = pCur->pNext;
-            }
-            eles
-            {
-                pPrev->pNext = pCur->pNext;
-            }
-            free(pCur);
-            return 0;
-        }
-        pPrev = pCur;
-        pCur = pCur->pNext;
-    }
-}
-
-template <class T>
-void List<T>::Display(int num)
-{
-    int counter = 1;
-    Node<T>* pCur = pHead;
-while(counter != num)
-{
-    cout << counter << ")" << pCur->word << endl;
-    cout << pCur->Description << endl;
-    ++counter;
-    pCur->pNext
-}
-}
